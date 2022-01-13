6d629a48b4b30907644ba9b63d6dd73781250005
jonahsaltzman <jonahsaltzman@gmail.com>
2022-01-13 18:16:16 -0500

    join a room

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
 const http = require('http')
 const Room = require('./room')
 
@@ -9,14 +7,25 @@ const server = http.createServer(app)
 
 module.exports = server
 
+const rooms = []
+
 app.get(
     '/newroom/:name',
     (req, res) => {
             console.log('creating room named: ', req.params.name)
-            Room.create(server, req.params.name)
+            const newRoom = Room.create(server, req.params.name)
+            rooms.push(newRoom)
             res.sendFile(__dirname + '/html/index.html')
         }
     )
+
+app.get(
+    '/join/:name',
+    (req, res) => {
+        const room = rooms.find(room => room.name = req.params.name)
+        res.sendFile(__dirname + '/html/index.html')
+    }
+)
 
 server.listen(3040, () => {
     console.log('listening on port 3040')
--- src/room.js
+++ src/room.js
@@ -7,6 +7,11 @@ class Room {
         this.server.on('connection', (socket) => {
             console.log('user connected')
             this.socket = socket
+            this.socket.name = name
+            //console.log(socket)
+            console.log(socket.id)
+            this.id = socket.id
+            console.log(socket.rooms)
             socket.on('chat message', (msg) => {
                 console.log('message: ', msg)
             })
@@ -15,6 +20,13 @@ class Room {
     static create(server, name) {
         const newServer = new this(server, name)
         console.log('created room')
+        return newServer
+    }
+    async addMember() {
+        console.log('request to join: ', this.name)
+        const joined = await this.socket.join(this.id)
+        console.log(joined)
+        return
     }
     #server
     #socket
